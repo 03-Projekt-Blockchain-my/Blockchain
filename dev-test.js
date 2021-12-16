c1c9edacec475d56745759ca07c7a039f0d7a581
ValleyofFeed <patrick.barkow@gmx.de>
2021-12-16 11:48:20 +0000

    blockchain geschaffen

--- dev-test.js
+++ dev-test.js
@@ -1,9 +1,26 @@
 const Block = require ("./block");
-
-//onst block = new Block("time","hash1","hash2","mydata");
+const Blockchain = require("./blockchain");
+//const block = new Block("time","hash1","hash2","mydata");
 
 
 //console.log(block.genesis().toString());
+// const testBlock = Block.mineBlock(Block.genesis(),"test");
+// console.log(testBlock.toString());
 
-const testBlock = Block.mineBlock(Block.genesis(),"test");
-console.log(testBlock.toString());
+// 1. + 2. Block
+
+const chain = new Blockchain();
+
+
+// 2. Block
+chain.addBlock("100€ an boo, 50$ von foo");
+
+
+// +3.Block
+
+chain.addBlock("50€ von boo");
+
+// +4. Block
+
+chain.addBlock("Bank an Kasse");
+console.log(chain);
